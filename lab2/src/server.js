7c79a8e0c74a02340ab140ad3aa04cd7d5ae6118
Dmytro Hushchin <dmytro.hushchin@alphanovel.io>
2023-02-25 22:38:04 +0200

    set up linter and prettier

--- lab2/src/server.js
+++ lab2/src/server.js
@@ -1,42 +1,45 @@
-import axios from "axios";
-import fs from "fs";
+import axios from 'axios';
+import * as fs from 'fs';
 import * as cheerio from 'cheerio';
 
 async function getHtml(url) {
     try {
         const response = await axios.get(url);
-        const html = response.data;
-        return html;
+        return response.data;
     } catch (error) {
         console.error(error);
+        return null;
     }
 }
 
 async function getArticles(html) {
     const $ = cheerio.load(html);
-    const articles = $(".css-1cp3ece").map((i, el) => {
-        const title = $(el).find("h2").text();
-        const link = $(el).find("a").attr("href");
-        const summary = $(el).find("p").text();
-        return { title, link, summary };
-    }).get();
-
-    return articles;
+    return $('.css-1cp3ece')
+        .map((i, el) => {
+            const title = $(el).find('h2').text();
+            const link = $(el).find('a').attr('href');
+            const summary = $(el).find('p').text();
+            return { title, link, summary };
+        })
+        .get();
 }
 
 async function saveData(articles) {
-    const data = JSON.stringify(articles, null, 2); // lenght = 3288
+    // data.lenght = 3288
+
+    if (!fs.existsSync('data')) {
+        fs.mkdirSync('data');
+    }
 
     for (let i = 0; i < 5; i++) {
         const filename = `data/article_${i}.json`;
         const article = articles[i];
-        // write to file
         fs.writeFileSync(filename, JSON.stringify(article, null, 2));
     }
 }
 
 async function main() {
-    const url = "https://www.nytimes.com/section/business";
+    const url = 'https://www.nytimes.com/section/business';
     const html = await getHtml(url);
     const articles = await getArticles(html);
     await saveData(articles);
