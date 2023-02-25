641694906810b2bd204a5ef893f9bcccaca1b085
Dmytro Hushchin <dmytro.hushchin@alphanovel.io>
2023-02-25 20:28:17 +0200

    html parsing

--- lab2/server.js
+++ lab2/server.js
@@ -1,4 +1,6 @@
 import axios from "axios";
+import fs from "fs";
+import * as cheerio from 'cheerio';
 
 async function getHtml(url) {
     try {
@@ -11,10 +13,23 @@ async function getHtml(url) {
     }
 }
 
+async function getArticles(html) {
+    const $ = cheerio.load(html);
+    const articles = $(".css-1cp3ece").map((i, el) => {
+        const title = $(el).find("h2").text();
+        const link = $(el).find("a").attr("href");
+        const summary = $(el).find("p").text();
+        return { title, link, summary };
+    }).get();
 
-setInterval(async () => {
-  const url = "https://www.nytimes.com/section/business";
-    const html = await getHtml(url);
-    console.log(html);
+    return articles;
 }
-, 5 * 1000);
+
+async function main() {
+    const url = "https://www.nytimes.com/section/business";
+    const html = await getHtml(url);
+    const articles = await getArticles(html);
+    console.log(articles);
+}
+
+main();
